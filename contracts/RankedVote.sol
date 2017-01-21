97a0518c32dc8675694ddc3e1865f9095e380a7a
Stephen Jackson <stephen.lee.jackson@googlemail.com>
2017-01-21 18:18:27 +0000

    Updating the solidity contract and tests
    Updating the solidity contract and tests

--- contracts/RankedVote.sol
+++ contracts/RankedVote.sol
@@ -8,13 +8,16 @@ contract RankedVote{
     string[] public votingGroupNames;
 
     struct Issue {
+        string name; // Description of the issue.
         uint256 voterGroup; // Voter group allowed on an issue.
         uint256 begin; // Timestamp the vote begins.
         uint256 end; // Timestamp the vote ends.
         bytes32[] proposals;
         uint256[][] votes; // List of list of order.
         mapping (address => bool) hasVoted;
+        uint256 winner;
     }
+
 
     Issue[] public issues;
 
@@ -47,19 +50,26 @@ contract RankedVote{
     }
 
     // Create an issue.
-    function createIssue(uint256 _voterGroup, uint256 _begin, uint256 _end, bytes32[] names) onlyChairman returns(uint256) {
+    function createIssue(uint256 _voterGroup, uint256 _begin, uint256 _end, string _name, bytes32[] names) onlyChairman returns(uint256) {
         if (_end < _begin) // Vote must last some time.
             throw;
-        if (_begin < now) // Can't start in the past
+        if (_begin < now) // Can't start in the past.
             throw;
 
         uint256 issueID = issues.length++;
-        issues[issueID].voterGroup=_voterGroup;
+        Issue issue = issues[issueID];
+
+        issue.name = _name;
+        issue.voterGroup = _voterGroup;
+        issue.begin = _begin;
+        issue.end = _end;
+
 
         for (uint256 i;i<names.length;++i)
             issues[issueID].proposals.push(names[i]);
         return issueID;
     }
+
 
 
 
@@ -72,9 +82,22 @@ contract RankedVote{
             throw;
         if (issue.end<now) // Vote is over.
             throw;
+        if (issue.begin>now) // Vote has not started yet.
+            throw;
+        if (order.length != issue.proposals.length) // Everything was not ranked.
+            throw;
 
         issue.votes.push(order); // Add the votes.
     }
+
+    // Set a new winner who would win his duel against the current one.
+    function condorcetImprove(uint256 issueID, uint256 betterProposal) {
+        Issue issue = issues[issueID];
+        if (getVictories(issueID,betterProposal, issue.winner) > getVictories(issue.winner, issueID,betterProposal) )
+            issue.winner = betterProposal; // If better proposal is effectively winning this duel, make it the winner.
+    }
+
+
 
 
 
@@ -82,10 +105,28 @@ contract RankedVote{
     // *     Constant Getters     * //
     // **************************** //
 
-    //function n() returns(uint256) constant {return now;}
+    function n() constant returns(uint256)  { return now; }
 
-    //function getProposalName() returns(string) constant {return "";}
+    function getProposalName(uint256 issueID, uint256 proposalID)  returns(bytes32)  { return issues[issueID].proposals[proposalID]; }
 
+    function getOrder(uint256 issueID, uint256 voteID, uint256 proposalID) constant returns(uint256) { return issues[issueID].votes[voteID][proposalID]; }
+
+    function getNumberIssues() constant returns(uint256) { return issues.length; }
+
+    function getNumberProposals(uint256 issueID) constant returns(uint256) { return issues[issueID].proposals.length; }
+
+
+
+
+    // Note that this gas use is unbounded.
+    // TODO: Make possibility to split this in multiple transactions.
+    function getVictories(uint256 issueID, uint256 proposalA, uint256 proposalB) constant returns(uint256) {
+        Issue issue = issues[issueID];
+        uint256 victories;
+        for (uint256 i;i<issue.proposals.length;++i) // Count the number of victories
+            victories += (issue.votes[i][proposalA]<issue.votes[i][proposalB] ? 1 : 0);
+        return victories;
+    }
 
 }
 
--- test/rankedvote.js
+++ test/rankedvote.js
@@ -1,36 +1,52 @@
 require('datejs');
 
 contract('RankedVote', function() {
+    var account_one = '0x540a0854d74c90c8269c891739b4e1938438a149';
+
     it("should not be possible to create an issue in the past", function() {
         var rankedVote = RankedVote.deployed();
-        var dateInThePast = Date.today().add(-1).days();
+        var start = Date.today().add(-1).days();
+        var end = start.add(2).days();
+        var description = "Test";
+        var names = ["0x0000000000000000000000", "0x0000000000000001"];
 
-        rankedVote.createIssue().then(function(issueId) {
-            assert.isNotNull(issueId, "Expected that the issue id would be returned.");
+        return rankedVote.createIssue(1, start.getTime(), end.getTime(), description, names, {from: account_one}).then(function(tx_id) {
             assert.isNotNull(rankedVote.issues, "Expected that the vote has issues.");
-            assert.isNotNull(rankedVote.issues[issueId], "Expected that the issue was not created for the vote.");
+            assert.equal(0, rankedVote.issues.length, "Expected there to be no issues in the contract.")
         });
     });
 
-    it("should not be able possible to create a contract with an end date as the start date", function() {
+    it("should not be able possible to create a contract with an end date the same as the start date", function() {
         var rankedVote = RankedVote.deployed();
-        console.log(Date.today().add(5).days());
+        var start = Date.today();
+        var end = start;
+        var description = "Test";
+        var names = ["0x0000000000000000000000", "0x0000000000000001"];
 
-        rankedVote.createIssue().then(function(issueId) {
-            assert.isNotNull(issueId, "Expected that the issue id would be returned.");
+        return rankedVote.createIssue(1, start.getTime(), end.getTime(), description, names, {from: account_one}).then(function(tx_id) {
             assert.isNotNull(rankedVote.issues, "Expected that the vote has issues.");
-            assert.isNotNull(rankedVote.issues[issueId], "Expected that the issue was not created for the vote.");
+            assert.equal(0, rankedVote.issues.length, "Expected there to be no issues in the contract.")
         });
     });
 
     it("should be able to add a create an issue as the chairman", function() {
         var rankedVote = RankedVote.deployed();
-        console.log(Date.today().add(5).days());
+        var start = Date.today();
+        var end = Date.today().add(1).days();
+        var description = "Test";
+        var names = ["0x0000000000000000000000", "0x0000000000000001"];
 
-        rankedVote.createIssue().then(function(issueId) {
-            assert.isNotNull(issueId, "Expected that the issue id would be returned.");
+        console.log('parameters: ' + 1 + ', ' + start.getTime() + ', ' + end.getTime() + ', ' + description + ', ' + names + ', ' + '{from: account_one, gas: 300000}');
+
+        return rankedVote.createIssue(0, start.getTime(), end.getTime(), description, names, {from: account_one, gas: 300000}).then(function(tx_id) {
             assert.isNotNull(rankedVote.issues, "Expected that the vote has issues.");
-            assert.isNotNull(rankedVote.issues[issueId], "Expected that the issue was not created for the vote.");
+            return rankedVote.issues.call(0).then(function (issue) {
+                assert.isNotNull(issue, "Expected there to be an issue created");
+                console.log('Issue details: ' + issue);
+                console.log('Issue start date: ' + issue.begin);
+            });
+
+
         });
     });
 });
